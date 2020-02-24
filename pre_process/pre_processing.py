ebd7e4b73b8ffe5d478b4d92a9a3bf6871d696d8
Cassio De Alcantara <cassio_alcantara@callink.com.br>
2020-02-24 15:35:28 -0300

    teste

--- pre_process/pre_processing.py
+++ pre_process/pre_processing.py
@@ -136,7 +136,8 @@ class Processing(object):
     def pre_processing(self):
 
         handler = DataHandler(self.social_network, self.search_words)
-        df = handler.read_network_dataset()
+        df_network  = handler.read_network_dataset()
+        df = df_network[df_network.tweets != '']
 
         nlp = spacy.load('pt')
         nltk.download("stopwords")
